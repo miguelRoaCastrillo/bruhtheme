7a77bc26f455dfb3e2fe34a1c1778b0c7037f304
Miguel Roa <miguelroacastrillo@gmail.com>
2023-06-12 22:53:40 -0400

    primer avance functions.php + styles.css

---
+++ functions.php
@@ -0,0 +1,26 @@
+<?php 
+    // Incluir Bootstrap CSS
+    function bootstrap_css() {
+        wp_enqueue_style( 
+            'bootstrap_css', 
+            get_stylesheet_directory_uri() . '/css/bootstrap.min.css', 
+            array(), 
+            '4.1.3'
+        ); 
+    }
+
+    add_action( 'wp_enqueue_scripts', 'bootstrap_css');
+
+    // Incluir Bootstrap JS
+    function bootstrap_js() {
+        wp_enqueue_script( 
+            'bootstrap_js', 
+            get_stylesheet_directory_uri() . '/js/bootstrap.min.js', 
+            array('jquery'), 
+            '4.1.3', 
+            true
+        ); 
+    }
+
+    add_action('wp_enqueue_scripts', 'bootstrap_js');
+?>
